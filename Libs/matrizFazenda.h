3969234480a0086e68f5c4daf3d70217a19c2b36
joaoVGvieira <joaovitorgv2019@gmail.com>
2022-11-06 15:53:30 -0300

    att

--- Libs/matrizFazenda.h
+++ Libs/matrizFazenda.h
@@ -7,7 +7,7 @@ typedef struct matrizFazenda{
     int colunas;
     int **matrizOriginal; //matriz lida do arquivo, nao ira mudar
     int **matrizPercorrida; // matriz zerada, que sera preenchida com a ordem da colheita das batatas
-    int caminhosPercorridos; // numero de campos de batats colhidos
+    int caminhosPercorridos; // numero de campos de batatas colhidas
 
 }matrizFazenda;
 
